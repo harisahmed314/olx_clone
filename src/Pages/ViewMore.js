13bfb77b2914b7833545ecb0385e35d81e50b729
harisahmed314 <harisahmed314@gmail.com>
2023-10-14 23:34:36 +0500

    this view more created

---
+++ src/Pages/ViewMore.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "../Componets/Header/Header"
+import AllPosts from '../Componets/ViewMoreAllPosts/AllPosts'
+
+function ViewMore() {
+    return (
+        <div>
+         <Header/>
+         <AllPosts/>        
+        </div>
+    )
+}
+
+export default ViewMore
---
+++ src/Pages/ViewPost.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Header from '../Componets/Header/Header'
+import View from '../Componets/View/View'
+
+
+function ViewPost(props) {
+    return (
+        <div>
+            
+            <Header />
+            <View/>
+            
+        </div>
+    )
+}
+
+export default ViewPost
